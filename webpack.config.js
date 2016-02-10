69f21de806c32c3f8e3b4316b429bd8fa37c189f
George Roots <groots@Georges-MacBook-Air.local>
2016-02-10 14:19:30 -0500

    trying to webpack configured more

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,7 @@
-var path = require('path');
-var webpack = require('webpack');
 module.exports = {
-	entry: './src/main.jsx',
+	entry: './src/main.js',
 	output: {
-		path: __dirname  + '/dist/',
+		path: './dist/',
 		filename: 'bundle.js'
-	},
-	module: {
-		loaders: [
-			{
-				test: /.jsx?$/,
-				loader: 'babel-loader',
-				exclude: /node_modules/,
-				query: {
-					presets: ['es2015', 'react']
-				}
-			}
-		]	
 	}
 }
